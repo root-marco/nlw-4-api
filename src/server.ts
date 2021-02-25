5cd7ba1021ad9d709f538b8f93c5be79d4d6007e
marco <rootslowed@gmail.com>
2021-02-25 14:59:17 -0300

    app post and app get to root route

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,14 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (req, res) => {
+	return res.json({ message: 'Hello, world' });
+});
+
+app.post('/', (req, res) => {
+	return res.json({ message: 'successfully saved' });
+});
+
 app.listen(3000, () => {
 	console.log('server is running');
 });
