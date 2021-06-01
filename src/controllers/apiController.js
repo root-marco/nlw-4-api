da1343aebaac62c6df3d1d7f74deb44a68aabe54
slowed <rootslowed@gmail.com>
2021-06-01 16:52:02 -0300

    logic getDate

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -10,27 +10,6 @@ export async function getHello(req, res) {
 
 export async function getDate(req, res) {
 
-  let date = new Date();
 
-  if (/^\d*$/.test(req.params.date)) {
-    date.setTime(req.params.date);
-  } else {
-    date = new Date(req.params.date);
-  }
-
-  res.set({
-    'Content-Type': 'application/json',
-  })
-
-  if (!date.getTime()) {
-    res.json({
-      error: "Invalid date given",
-    });
-  } else {
-    res.json({
-      unix: date.getTime(),
-      utc: strftime('%B %d, %Y', date),
-    });
-  }
 
 }
